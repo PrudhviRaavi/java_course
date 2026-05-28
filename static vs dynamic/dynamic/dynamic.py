d3db652303a967df5241dba150f7845b84295017
Prudhvi Krishna Raavi <raaviprudhvi1@gmail.com>
2026-05-28 12:29:09 +0530

    static vs dynamic diff

---
+++ static vs dynamic/dynamic/dynamic.py
@@ -0,0 +1,15 @@
+#for dynamic we cannot need to declare the type of variable before using it. we can directly assign the value to the variable and it will automatically detect the type of variable.
+
+a=10
+print(a)
+print(type(a))
+
+#python and javascript are dynamic programming languages because we can directly assign the value to the variable without declaring the type of variable. and it will automatically detect the type of variable.
+
+a="Hello"
+print(a)
+print(type(a))
+
+a=10.5
+print(a)
+print(type(a))
---
+++ static vs dynamic/static/statically.java
@@ -0,0 +1,13 @@
+import java.lang.*;
+class statically
+{
+    public static void main(String args[])
+    {
+        // for statically typed language we have to declare the data type of variable before using it
+        int a=10;
+        System.out.println("The value of a is "+a);
+        // static language are java, c, c++, c#, etc
+        String b = "Hello";
+        System.out.println("The Name of b is "+b);
+    }
+}
